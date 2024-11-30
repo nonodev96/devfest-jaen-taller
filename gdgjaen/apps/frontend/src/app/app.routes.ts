da1fe712fde19636c8485ca04c570e5b792c540a
Antonio Mudarra Machuca <nonodev96@gmail.com>
2024-11-30 12:32:49 +0000

    end

--- gdgjaen/apps/frontend/src/app/app.routes.ts
+++ gdgjaen/apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { PonentesComponent } from '@gdgjaen/ponentes';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'ponentes',
+        loadComponent: () => import('@gdgjaen/ponentes').then(()=> PonentesComponent)
+    }
+];
